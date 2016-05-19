d72649eebf3e2e6a2961f2c66e534c2fddbe4f51
jasssim2nine <jasim.khan89@gmail.com>
2016-05-19 10:44:30 -0400

    added site master page without nav

---
+++ COMP-2007-S2016-L1P2A/Site.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COMP_2007_S2016_L1P2A
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
